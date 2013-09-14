6f93ecdf6ebf3545489cfac45927e707b1870c5c
Yihui Xie <xie@yihui.name>
2013-09-14 15:40:12 -0500

    the initial version (perhaps also the last version?)

---
+++ R/serve.R
@@ -0,0 +1,54 @@
+#' Serve the static files under a directory
+#'
+#' If there is an \file{index.html} under this directory, it will be displayed;
+#' otherwise the list of files is displayed, with links on their names. After we
+#' run this function, we can go to \url{http://localhost:port} to browse the web
+#' pages either created from R or read from HTML files.
+#' @param dir the directory to serve
+#' @param port the TCP port number; by default it is \code{4321}, unless a
+#'   command line argument of the form \code{-pNNNN} (N is a digit from 0 to 9)
+#'   was passed in when R was launched, in which case \code{NNNN} will be used
+#'   as the port number
+#' @param launch.browser whether to launch the default web browser; by default,
+#'   it is \code{TRUE} if the R session is \code{\link{interactive}()}, or when
+#'   a command line argument \code{-b} was passed to R
+#' @export
+#' @importFrom httpuv runServer
+#' @examples #' see https://github.com/yihui/servr for command line usage
+#' # or run inside an R session
+#' if (interactive()) servr::httd()
+httd = function(dir = '.', port, launch.browser) {
+  if (dir != '.') {
+    owd = setwd(dir); on.exit(setwd(owd))
+  }
+  cargs = commandArgs(TRUE)
+  if (missing(launch.browser)) launch.browser = interactive() || '-b' %in% cargs
+  if (missing(port))
+    port = if (length(port <- grep('^-p[0-9]{4,}$', cargs, value = TRUE)) == 1)
+      as.integer(sub('^-p', '', port)) else 4321L
+  library(methods)
+  url = sprintf('http://localhost:%d', port)
+  if (launch.browser) browseURL(url) else message('serving the directory ', dir, ' at ', url)
+  runServer('0.0.0.0', port, list(call = serve_dir))
+}
+
+serve_dir = function(req) {
+  path = paste('.', req$PATH_INFO, sep = '')  # the requested file
+  body = if (file_test('-d', path)) {
+    type = 'text/html'
+    if (file.exists(idx <- file.path(path, 'index.html'))) readLines(idx) else {
+      d = file.info(list.files(path, all.files = TRUE, full.names = TRUE))
+      title = escape_html(path)
+      html_doc(c(sprintf('<h1>Index of %s</h1>', title), fileinfo_table(d)),
+               title = title)
+    }
+  } else {
+    if (!file.exists(path))
+      return(list(status = 404L, headers = list('Content-Type' = 'text/plain'),
+                  body = paste('Not found:', path, '\r\n')))
+    type = guess_mime(path)
+    readBin(path, 'raw', file.info(path)[, 'size'])
+  }
+  if (is.character(body) && length(body) > 1) body = paste(body, collapse='\r\n')
+  list(status = 200L, headers = list('Content-Type' = type), body = body)
+}
---
+++ R/utils.R
@@ -0,0 +1,130 @@
+# copied from the markdown package: http://cran.r-project.org/package=markdown
+.MIMEMAP <- list(
+
+  ## most common web types
+  htm = 'text/html',
+  html = 'text/html',
+  css = 'text/css',
+  gif = 'image/gif',
+  jpg = 'image/jpeg',
+  jpeg = 'image/jpeg',
+  jpe = 'image/jpeg',
+  png = 'image/png',
+  js = 'application/x-javascript',
+  pdf = 'application/pdf',
+  svg = 'image/svg+xml',
+  swf = 'application/x-shockwave-flash',
+
+  ## markdown types
+  md = 'text/x-markdown',
+  mdtxt = 'text/x-markdown',
+  markdown = 'text/x-markdown',
+
+  ## other types we are likely to serve
+  xml = 'text/xml',
+  csv = 'text/csv',
+  ico = 'image/x-icon',
+  zip = 'application/zip',
+  bz = 'application/x-bzip',
+  bz2 = 'application/x-bzip2',
+  gz = 'application/x-gzip',
+  tar = 'application/x-tar',
+
+  ## yet more types...
+
+  shtml = 'text/html',
+  tsv = 'text/tab-separated-values',
+  tab = 'text/tab-separated-values',
+  dcf = 'text/debian-control-file',
+  txt = 'text/plain',
+  mml = 'text/mathml',
+
+  tif = 'image/tiff',
+  tiff = 'image/tiff',
+  bmp = 'image/bmp',
+  ps = 'application/postscript',
+  eps = 'application/postscript',
+  dvi =   'application/x-dvi',
+
+  atom = 'application/atom+xml',
+  rss = 'application/rss+xml',
+
+  doc = 'application/msword',
+  docx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  odt = 'application/vnd.oasis.opendocument.text',
+  rtf = 'application/rtf',
+  xls = 'application/vnd.ms-excel',
+  xlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  ods = 'application/x-vnd.oasis.opendocument.spreadsheet',
+  ppt = 'application/vnd.ms-powerpoint',
+  pps = 'application/vnd.ms-powerpoint',
+  pptx = 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+
+  sit = 'application/x-stuffit',
+  sxw = 'application/vnd.sun.xml.writer',
+
+  iso = 'application/octet-stream',
+  dmg = 'application/octet-stream',
+  exe = 'application/octet-stream',
+  dll = 'application/octet-stream',
+  deb = 'application/octet-stream',
+  otf = 'application/octet-stream',
+  xpi = 'application/x-xpinstall',
+
+  mp2 = 'audio/mpeg',
+  mp3 = 'audio/mpeg',
+
+  mpg = 'video/mpeg',
+  mpeg = 'video/mpeg',
+  flv = 'video/x-flv'
+)
+
+guess_mime = function(file) {
+  ext = tolower(tools::file_ext(file))
+  # assume files without extension are plain text files, so are R, Rd
+  if (ext %in% c('', 'r', 'rd')) return('text/plain')
+  if (is.null(.MIMEMAP[[ext]])) 'application/octet-stream' else .MIMEMAP[[ext]]
+}
+
+# turn() file.info() to an HTML table
+fileinfo_table = function(info) {
+  info = info[order(info$isdir, decreasing = TRUE), ]
+  d = info$isdir; i = !is.na(d)
+  # files/dirs
+  x1 = paste(basename(rownames(info)), ifelse(d & i, '/', ''), sep = '')
+  x1 = escape_html(x1)
+  x1[i] = sprintf('<a href="%s">%s</a>', x1[i], x1[i])
+  # size
+  x2 = paste(format(info$size, scientific = FALSE, big.mark = ','), 'B')
+  x2[is.na(info$size) | d] = ''
+  # date modified
+  x3 = as.character(info$mtime)
+  x3[is.na(x3)] = ''
+  c('<table>',
+    '<thead><tr>',
+    sprintf('<th>%s</th>', c('Name', 'Size', 'Date Modified')),
+    '</tr></thead>',
+    apply(cbind(
+      '<tr>',
+      sprintf('<td>%s</td>', x1),
+      sprintf('<td align="right">%s</td>', x2),
+      sprintf('<td>%s</td>', x3),
+      '</tr>'), 1, paste, collapse = ''),
+    '</table>')
+}
+
+# make an HTML document from body and title
+html_doc = function(body, title = NULL) {
+  c('<!DOCTYPE html>', '<html>',
+    '<head>', sprintf('<title>%s</title>', title), '</head>',
+    '<body>', body, '</body>', '</html>')
+}
+
+# escape special HTML chars (copied from highr)
+escape_html = function(x) {
+  x = gsub('&', '&amp;', x)
+  x = gsub('<', '&lt;', x)
+  x = gsub('>', '&gt;', x)
+  x = gsub('"', '&quot;', x)
+  x
+}

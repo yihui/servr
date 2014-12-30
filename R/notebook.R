4adf3af18434b21f7cb8bc52067958d05c8ccca9
Yihui Xie <xie@yihui.name>
2014-12-30 15:13:13 -0600

    a simple rnotebook server (render markdown using CommonMark for now)
    closes yihui/rnotebook#3

---
+++ R/notebook.R
@@ -0,0 +1,34 @@
+#' Serve R notebooks
+#'
+#' Serve R notebook files created from the \pkg{rnotebook} package.
+#'
+#' An R notebook is basically a JSON file, and the JSON data is inserted to a
+#' predefined template in this package (\code{system.file('resources',
+#' 'rnotebook.html', package = 'servr')}), and rendered through the JavaScript
+#' implementation of \href{http://commonmark.org}{CommonMark} at the moment (the
+#' renderer may change in the future).
+#' @inheritParams httd
+#' @inheritParams server_config
+#' @export
+#' @examples # see ?servr::newnb for examples
+notebook = function(dir = '.', ...) {
+  dir = normalizePath(dir, mustWork = TRUE)
+  in_dir(dir, {
+    build = build_watcher('[.]Rnb$', rnotebook::refresh)
+  })
+  dynamic_site(
+    dir, ..., site.dir = '.',
+    build = build,
+    response = function(req) {
+      path = sub('^/', '', req$PATH_INFO)
+      if (!grepl('[.]Rnb$', path) || !file.exists(path)) return(serve_dir()(req))
+      json  = readLines(path, encoding = 'UTF-8')
+      tmpl  = readLines(system.file('resources', 'rnotebook.html', package = 'servr'))
+      token = '%RNOTEBOOK_DATA%'
+      if (length(i <- which(token == tmpl)) != 1) stop('Wrong notebook template')
+      tmpl[i] = paste(json, collapse = '\n')
+      tmpl = paste(tmpl, collapse = '\r\n')
+      list(status = 200L, headers = list('Content-Type' = 'text/html'), body = tmpl)
+    }
+  )
+}

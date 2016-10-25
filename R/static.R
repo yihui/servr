yihui/servr

commit 8e8c4fd5facdbddc64a4f8f05fccebf4e5f18e2d

fixes #10: no need to redirect to ./ when the requested path is the root directory .
otherwise it will cause infinite redirection loop in Chrome/Firefox

R/static.R
@@ -93,8 +93,10 @@ serve_dir = function(dir = '.') function(req) {
   } else if (path == '') path = '.'
   body = if (file_test('-d', path)) {
     # ensure a trailing slash if the requested dir does not have one
-    if (!grepl('/$', path)) return(list(
-      status = 301L, body = '', headers = list('Location' = sprintf('%s/', req$PATH_INFO))
+    if (path != '.' && !grepl('/$', path)) return(list(
+      status = 301L, body = '', headers = list(
+        'Location' = sprintf('/%s/', req$PATH_INFO)
+      )
     ))
     type = 'text/html'
     if (file.exists(idx <- file.path(path, 'index.html'))) readLines(idx) else {

commit 55f591a2b88c19de207796cae31bebab6072adc1

should not prepend / in the path
e.g. if the requested path was /2016/10/foo, the redirection will be to //2016/10/foo/ and this is problematic, since //2016 will be resolved to http://0.0.7.224

the bug was introduced by 8e8c4fd5facdbddc64a4f8f05fccebf4e5f18e2d

R/static.R
@@ -143,7 +143,7 @@ serve_dir = function(dir = '.') function(req) {
     # ensure a trailing slash if the requested dir does not have one
     if (path != '.' && !grepl('/$', path)) return(list(
       status = 301L, body = '', headers = list(
-        'Location' = sprintf('/%s/', req$PATH_INFO)
+        'Location' = sprintf('%s/', req$PATH_INFO)
       )
     ))
     type = 'text/html'
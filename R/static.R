5e3b81e86064f227818c4b8424d0aca851a302a2
Yihui Xie <xie@yihui.name>
2018-07-03 16:03:33 -0500

    support the global option servr.port for the port argument of server_config()

--- R/static.R
+++ R/static.R
@@ -115,7 +115,7 @@ server_config = function(
   if (missing(browser)) browser = interactive() || '-b' %in% cargs || is_rstudio()
   if (missing(port))
     port = if (length(port <- grep('^-p[0-9]{4,}$', cargs, value = TRUE)) == 1)
-      as.integer(sub('^-p', '', port)) else random_port()
+      as.integer(sub('^-p', '', port)) else getOption('servr.port', random_port())
   if (missing(daemon)) daemon = getOption('servr.daemon', ('-d' %in% cargs) || interactive())
   damn_library('methods')
   url = sprintf('http://%s:%d', host, port)

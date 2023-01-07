d9a66ebd380b896d4b8093f5762d4c7a370ee902
Mentik <mentikyusmantara@gmail.com>
2023-01-08 00:35:07 +0800

    fix error

--- inst/examples/docker/app.R
+++ inst/examples/docker/app.R
@@ -2,6 +2,7 @@ require("servr")
 
 writeLines("\n\n\n *** Starting Servr *** \n\n\n")
 
-servr::create_server(port = 2222, handler = function(req) {
-    list(status = 200L, body = paste("Success:", req$PATH_INFO))
-  })
+s = servr::create_server(port = 2222, host = '0.0.0.0', handler = function(req) {
+  list(status = 200L, body = paste("Success:", req$PATH_INFO))
+})
+print(s$url)
